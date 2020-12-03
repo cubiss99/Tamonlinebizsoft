e0826e8a2393cf988cacdb042cb5b68031781405
cubiss99 <74998526+cubiss99@users.noreply.github.com>
2020-12-03 11:52:48 +0700

    Update ballbounce.js

--- ball/ballbounce.js
+++ ball/ballbounce.js
@@ -1,17 +1,24 @@
-
+// lấy được thẻ <canvas> trong body của html
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+//bán kính bóng
 var ballRadius = 10;
+
+// góc tọa độ đầu tiên + tốc bộ ban đầu 
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+//khả năng có thể move trái và phải
 var rightPressed = false;
 var leftPressed = false;
+
+// add event keyup keydowm
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
+//sự kiện keydown được kích hoạt
 function keyDownHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = true;
@@ -21,6 +28,7 @@ function keyDownHandler(e) {
     }
 }
 
+//sự kiện keyup được kích hoạt
 function keyUpHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = false;
@@ -30,25 +38,32 @@ function keyUpHandler(e) {
     }
 }
 
+// beginPath+ closePasth là hướng 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);// hàm vẽ bóng
+    // fillStyle định nghĩa màu sử dụng cho fill
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
+// Xóa Canavas trước khi tạo frame mới
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-   
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    
+    
+   // bóng di chuyển trái và phải
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) { 
         dx = -dx;
     }
+    
+    // bóng di chuyển lên và xuống
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
-    
+    // công thức di chuyển bóng
     if(rightPressed) {
         dx += 4;
     }
@@ -60,4 +75,5 @@ function draw() {
     y += dy;
 }
 
+// hàm draw thực thi mỗi 10 mili giây.
 setInterval(draw, 10);
